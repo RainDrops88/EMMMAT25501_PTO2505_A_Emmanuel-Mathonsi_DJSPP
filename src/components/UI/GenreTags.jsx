c58990856002a1ae19070e9c2688225db7ec6c41
Emmanuel <07942jerry@gmail.com>
2026-04-04 20:15:34 +0200

    fixing the function to get genres fron podcast context

--- src/components/UI/GenreTags.jsx
+++ src/components/UI/GenreTags.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { genres as genreMap } from "../../data";
+import { useContext } from "react";
+import { PodcastContext } from "../../context/PodcastContext";
 import styles from "./GenreTags.module.css";
 
 /**
  * GenreTags component.
  *
  * Displays a list of genre tags based on an array of genre IDs.
- * It maps each ID to its corresponding title using the local genre dataset.
+ * It maps each ID to its corresponding title using the fetched genre dataset.
  * Unrecognized IDs are labeled as "Unknown".
  *
  * @param {Object} props
@@ -14,14 +14,18 @@ import styles from "./GenreTags.module.css";
  *
  * @returns {JSX.Element} A styled list of genre tags.
  */
-export default function GenreTags({ genres }) {
+export default function GenreTags({ genres = [] }) {
+  const { genres: genreMap = [] } = useContext(PodcastContext);
+
   const genreSpans = genres.map((id) => {
     const match = genreMap.find((genre) => genre.id === id);
+
     return (
       <span key={id} className={styles.tag}>
         {match ? match.title : `Unknown (${id})`}
       </span>
     );
   });
+
   return <div className={styles.tags}>{genreSpans}</div>;
 }
