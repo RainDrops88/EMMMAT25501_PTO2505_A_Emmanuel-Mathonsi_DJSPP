c50a39c69537a6ed47658c1b9a199b47c3aa88d1
Emmanuel <07942jerry@gmail.com>
2026-04-05 18:56:34 +0200

    linking favorite

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/podcast icon.webp";
 import styles from "./Header.module.css";
+import Favorite from "../Filters/FavoriteCard";
 
 export default function Header({ theme, onToggleTheme }) {
   const isDarkTheme = theme === "dark";
@@ -10,6 +11,9 @@ export default function Header({ theme, onToggleTheme }) {
       <Link to="/" className={styles.brand}>
         <img className={styles.logo} src={logo} alt="" />
         <span>Podcast App</span>
+      </Link>
+      <Link to="/favorites" className={styles.favoriteLink}>
+        <Favorite />
       </Link>
 
       <button
